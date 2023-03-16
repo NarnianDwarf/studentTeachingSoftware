527a9d8f5c02107c19f8622e977ec132ad1872dc
NarnianDwarf <117846764+NarnianDwarf@users.noreply.github.com>
2023-03-16 02:09:54 -0400

    Working to solve id bug
    currently the stud_id is the user_id for evaluation sorting.  Which is crappy.  Working on fixing that.

--- mysite/evalFirst/admin.py
+++ mysite/evalFirst/admin.py
@@ -13,12 +13,18 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.unregister(User)
+#COULD THIS BE THE ISSUE????
 admin.site.register(User, UserAdmin)
 admin.site.register(firstEvaluation)
 # admin.site.register(Profile)
 
 #mix user and profile info
 
+
+#TWO IDEAS:
+
+#1. I make a value that is a student id associated with each user
+#2. I make a dropdown list of users????
 
 
 
